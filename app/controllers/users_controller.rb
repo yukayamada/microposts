b85c8e82b032b940d434ceb41ac05ddd97119909
yukayamada <yyamada@netprotections.co.jp>
2016-06-17 01:12:50 +0000

    modified

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :check_user, only: [:edit, :update]
+
+  
   def show
    @user = User.find(params[:id])
   end
@@ -33,7 +36,12 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :location,
+                                 :password, :password_confirmation)
+  end
+  
+  def check_user
+    @user = User.find(params[:id])
+    redirect_to root_path if current_user != @user
   end
 end
