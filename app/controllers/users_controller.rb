18edb008cf752d55c72ab5264b6462a42642a3c2
yukayamada <yyamada@netprotections.co.jp>
2016-06-20 00:46:07 +0000

    followings-followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,16 @@ class UsersController < ApplicationController
     end
   end
   
+  def followings
+    @user = User.find(params[:id])
+    @users = @user.following_users
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.follower_users
+  end
+  
   private
 
   def user_params
